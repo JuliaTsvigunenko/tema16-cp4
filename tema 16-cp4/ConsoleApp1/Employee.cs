6533ab16ccaf66b329cf6c0662c63bcf2362ea24
JuliaTsvigunenko <122933386+JuliaTsvigunenko@users.noreply.github.com>
2024-02-04 14:45:27 +0200

    Add files via upload

---
+++ tema 16-cp4/ConsoleApp1/Employee.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Employee
+    {
+
+        /// <summary>
+        /// Поле табельного номера
+        /// </summary>
+        private int personnelNumber;
+
+        /// <summary>
+        /// Поле ФИО сотрудника
+        /// </summary>
+        private string fullName;
+
+        /// <summary>
+        /// Поле дата рождения сотрудника
+        /// </summary>
+        private DateTime birthday;
+
+        /// <summary>
+        /// Поле должности сотрудника
+        /// </summary>
+        private string jobTitle;
+
+        /// <summary>
+        /// Поле оклада сотрудника
+        /// </summary>
+        private int salary;
+
+
+        /// <summary>
+        /// Конструктор с параметрами
+        /// </summary>
+        /// <param name="personnelNumber"></param>
+        /// <param name="fullName"></param>
+        /// <param name="birthday"></param>
+        /// <param name="jobTitle"></param>
+        /// <param name="salary"></param>
+        public Employee(int personnelNumber, string fullName, DateTime birthday, string jobTitle, int salary)
+        {
+            this.personnelNumber = personnelNumber;
+            this.fullName = fullName;
+            this.birthday = birthday;
+            this.jobTitle = jobTitle;
+            this.salary = salary;
+
+        }
+
+
+        /// <summary>
+        /// Поле свойства для доступа к полям Гет и Сет
+        /// </summary>
+        public int PersonnelNumber { get { return personnelNumber; } set { personnelNumber = value; } }
+        public string FullName { get { return fullName; } set { fullName = value; } }
+        public DateTime Birthday { get { return birthday; } set { birthday = value; } }
+        public string JobTitle { get { return jobTitle; } set { jobTitle = value; } }
+        public int Salary { get { return salary; } set { salary = value; } }
+
+
+        /// <summary>
+        /// Метод для вывода данных о сотруднике
+        /// </summary>
+        public void ShowInfo()
+        {
+            Console.WriteLine($"\tТабельный номер: {personnelNumber}\n ФИО сотрудника: {fullName}\n Дата рождениясотрудника: {birthday}\n Должность сотрудника: {jobTitle}\n Оклад сотрудника: {salary}");
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ tema 16-cp4/ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Создаем объекты сотрудников
+            //Employees employees1 = new Employees(1, "Цвигуненко Юлия Владимировна", new DateTime(2006, 5, 19), "Дизайнер", 10000);
+            //Employees employees2 = new Employees(2, "Лебедева Серафима Игоревна", new DateTime(2007, 7, 1), "Программист", 10000);
+            //Employees employees3 = new Employees(3, "Крижановская Арина Владимировна", new DateTime(2006, 5, 17), "Повар", 10000);
+
+            //Console.WriteLine("Информация о сотруднике 1: ");
+            //employees1.ShowInfo();
+
+            //Console.WriteLine("Информация о сотруднике 2: ");
+            //employees2.ShowInfo();
+
+            //Console.WriteLine("Информация о сотруднике 3: ");
+            //employees3.ShowInfo();
+
+
+
+            // Создать массив из N объектов данной структуры.
+            // Сведения об объектах вывести в табличном виде.
+            // В случае отсутствия информации об объектах, вывести соответствующее сообщение.
+
+            Console.Write("n= ");
+            int n = int.Parse(Console.ReadLine());
+            Employee[] emp = new Employee[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Инфомация о сотруднике {i + 1}: ");
+                Console.Write("Табельный номер: ");
+                int personnelNumber = int.Parse(Console.ReadLine());
+                Console.Write("ФИО сотрудника: ");
+                string fullName = Console.ReadLine();
+                Console.Write("Дата рождения: ");
+                DateTime birthday = DateTime.Parse(Console.ReadLine());
+                Console.Write("должность: ");
+                string jobTitle = Console.ReadLine();
+                Console.Write("оклад: ");
+                int salary = Convert.ToInt32(Console.ReadLine());
+
+                emp[i] = new Employee(personnelNumber, fullName, birthday, jobTitle, salary);
+
+            }
+
+
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
